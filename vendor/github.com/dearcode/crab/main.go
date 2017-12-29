a8b1cb9c95066c8f401c55f66694e24a806e73b5
c-wind <mrt@codecn.org>
2017-12-29 14:34:10 +0800

    添加dep

---
+++ vendor/github.com/dearcode/crab/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/dearcode/crab/http/client"
+	"github.com/dearcode/crab/http/server"
+	_ "github.com/dearcode/crab/server"
+)
+
+type index struct {
+	r *http.Request
+}
+
+func (i *index) GET(w http.ResponseWriter, req *http.Request) {
+	i.r = req
+	w.Write([]byte(fmt.Sprintf("client:%v addr:%p", i.r.RemoteAddr, i)))
+}
+
+func testHTTPClient() {
+	url := "http://127.0.0.1:9000/main/index/"
+	buf, _, err := client.New(time.Second).Get(url, nil, nil)
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Printf("response:%s\n", buf)
+}
+
+func main() {
+	addr := flag.String("h", ":9000", "api listen address")
+	flag.Parse()
+
+	server.Register(&index{})
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			time.Sleep(time.Second)
+			testHTTPClient()
+		}
+	}()
+
+	if err := server.Start(*addr); err != nil {
+		panic(err.Error())
+	}
+}
